795655614eae87147bc566f3b15aeed5fe692cd7
Alifian <alifiancandra93@gmail.com>
2026-03-13 22:33:21 +0700

    fetch prouct data from api

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 const productContainer = document.getElementById("productContainer")
 
 console.log(productContainer)
+
+fetch("https://fakestoreapi.com/products")
+.then(response => response.json())
+.then(data => {
+console.log(data)
+})
