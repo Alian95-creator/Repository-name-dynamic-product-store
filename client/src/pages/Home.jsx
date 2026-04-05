e73ba2924472b1a290d895f7aa1e2a6eef282387
Alifian <alifiancandra93@gmail.com>
2026-04-05 17:24:37 +0700

    feat: implement search and filter

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -21,19 +21,7 @@ const Home = () => {
     ...new Set(products.map((p) => p.category)),
   ];
 
-  if (loading) {
-  return (
-    <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
-      {[...Array(8)].map((_, i) => (
-        <div key={i} className="bg-white p-4 rounded-xl animate-pulse">
-          <div className="h-40 bg-gray-300 rounded mb-3"></div>
-          <div className="h-4 bg-gray-300 rounded mb-2"></div>
-          <div className="h-4 bg-gray-200 rounded w-1/2"></div>
-        </div>
-      ))}
-    </div>
-  );
-}
+<q></q>
 
   return (
     <div>
